96f63f599bb5f3163ee0140dc0c9fc4e214aa79f
JunSeok Kim <skaksdl1238@gmail.com>
2022-12-29 23:46:39 +0900

    commit - Login Api

--- src/main/java/com/example/graduation/member/controller/MemberController.java
+++ src/main/java/com/example/graduation/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.graduation.member.controller;
 
 import com.example.graduation.member.dto.MemberRequestDto;
 import com.example.graduation.member.repository.MemberRepository;
+import com.example.graduation.member.service.LoginService;
 import com.example.graduation.member.service.SignUpSerivce;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,17 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final SignUpSerivce signUpSerivce;
+    private final LoginService loginService;
     private final MemberRepository memberRepository;
 
 
     @PostMapping("/signUp")
     public Long signUp(@RequestBody MemberRequestDto memberRequestDto) throws Exception{
         return signUpSerivce.signUp(memberRequestDto);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody String email, String password){
+        return loginService.isValidMemberIdAndPassword(email,password);
     }
 }

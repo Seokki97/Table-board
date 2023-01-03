f20d1e289d1d78461c816fb002ac506292f2a4c6
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-04 00:47:03 +0900

    commit - util부분 cast 오류나서 디버깅

--- src/main/java/com/example/graduation/member/config/SecurityUtil.java
+++ src/main/java/com/example/graduation/member/config/SecurityUtil.java
@@ -1,11 +1,12 @@
 package com.example.graduation.member.config;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
     public static String getLoginUserName(){
-        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return user.getUsername();
+        Authentication user = SecurityContextHolder.getContext().getAuthentication();
+        return user.getName();
     }
+
 }

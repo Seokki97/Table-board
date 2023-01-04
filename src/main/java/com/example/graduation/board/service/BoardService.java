9a7360421131f4f4140552d945d0ed9aa1489852
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-05 01:39:42 +0900

    commit - 게시글찾기, 게시글 리스트 조회 서비스코드 생성

--- src/main/java/com/example/graduation/board/service/BoardService.java
+++ src/main/java/com/example/graduation/board/service/BoardService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +23,28 @@ public class BoardService {
         return boardRepository.save(board);
 
     }
+
+    //해당 제목과 일치하는 board 반환
+    public Board findPostWithTitle(BoardRequestDto boardRequestDto){
+        if(!boardRepository.existsByTitle(boardRequestDto.getTitle())){
+        throw new IllegalArgumentException("해당 제목과 일치한 게시물이 없습니다.");
+        }
+        return boardRepository.findByTitle(boardRequestDto.getTitle()).get();
+    }
+    //해당 내용과 일치하는 board 반환
+    public Board findPostWithContent(BoardRequestDto boardRequestDto){
+        if(!boardRepository.existsByContent(boardRequestDto.getContent())){
+            throw new IllegalArgumentException("해당 내용과 일치한 게시물이 없습니다");
+        }
+        return boardRepository.findByContent(boardRequestDto.getContent()).get();
+    }
+
+    //게시글 리스트 전체조회
+    public List<Board> showAllPostList(){
+        List<Board> boardList = boardRepository.findAll();
+
+        return  boardList;
+    }
+
+
 }

d136db2c39f49c659714f639fe9ae094075296fb
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-07 20:11:16 +0900

    refactor - 로그아웃api 추가

--- src/main/java/com/example/graduation/member/controller/MemberController.java
+++ src/main/java/com/example/graduation/member/controller/MemberController.java
@@ -1,11 +1,18 @@
 package com.example.graduation.member.controller;
 
+import com.example.graduation.member.config.SecurityConfig;
 import com.example.graduation.member.config.SecurityUtil;
 import com.example.graduation.member.dto.MemberRequestDto;
 import com.example.graduation.member.service.LoginService;
 import com.example.graduation.member.service.SignUpSerivce;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @RequestMapping("member")
@@ -15,12 +22,10 @@ public class MemberController {
     private final SignUpSerivce signUpSerivce;
     private final LoginService loginService;
 
-
     @PostMapping("/signUp")
     public Long signUp(@RequestBody MemberRequestDto memberRequestDto) throws Exception {
         return signUpSerivce.signUp(memberRequestDto);
     }
-
 
     @PostMapping("/login")
     public String login(@RequestBody MemberRequestDto memberRequestDto) {
@@ -28,15 +33,12 @@ public class MemberController {
 
     }
 
-    @PostMapping("/test")
-    public String test(){
-            return "성공이요";
-        }
-/*
-    @PostMapping("/logout")
-    public void logout(HttpServletResponse httpServletResponse){
-        loginService.logoutMember(httpServletResponse);
-    }*/
+    @GetMapping("/logout")
+    public ResponseEntity<Boolean> logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        new SecurityContextLogoutHandler()
+                .logout(httpServletRequest, httpServletResponse, SecurityContextHolder.getContext().getAuthentication());
+        return ResponseEntity.ok().body(true);
+    }
 
     @GetMapping("/userInfo")
     public String getUserInfo() {

2d8335af2c5ceb29af94028cdad85504351b2dda
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-07 20:18:27 +0900

    feat - api 주석

--- src/main/java/com/example/graduation/board/service/BoardService.java
+++ src/main/java/com/example/graduation/board/service/BoardService.java
@@ -21,17 +21,16 @@ public class BoardService {
 
     private final MemberRepository memberRepository;
 
-    private final LoginService loginService;
+    //게시물 작성 api
     public Board writePost(Long memberId, BoardRequestDto boardRequestDto) {
 
-        //멤버아이디를 모르는데 어케찾아?
         Member member = memberRepository.findById(memberRepository.findById(memberId).get().getId()).get();
         boardRequestDto.setCreatedDate(LocalDateTime.now());
         Board board = boardRepository.save(boardRequestDto.toEntity(member));
         return board;
     }
 
-    //해당 제목과 일치하는 board 반환
+    //해당 제목과 일치하는 board 반환 api
     public Board findPostWithTitle(BoardRequestDto boardRequestDto) {
         if (!boardRepository.existsByTitle(boardRequestDto.getTitle())) {
             throw new IllegalArgumentException("해당 제목과 일치한 게시물이 없습니다.");
@@ -40,7 +39,7 @@ public class BoardService {
         return boardRepository.findByTitle(boardRequestDto.getTitle()).get();
     }
 
-    //해당 내용과 일치하는 board 반환
+    //해당 내용과 일치하는 board 반환 api
     public Board findPostWithContent(BoardRequestDto boardRequestDto) {
         if (!boardRepository.existsByContent(boardRequestDto.getContent())) {
             throw new IllegalArgumentException("해당 내용과 일치한 게시물이 없습니다");
@@ -48,21 +47,21 @@ public class BoardService {
         return boardRepository.findByContent(boardRequestDto.getContent()).get();
     }
 
-    //게시글 리스트 전체조회
+    //게시글 리스트 전체조회 api
     public List<Board> showAllPostList() {
         List<Board> boardList = boardRepository.findAll();
 
         return boardList;
     }
 
-    //게시글 단일 조회 -> 게시물 제목을 클릭하면 해당 게시물의 내용을 보내주면됨
+    //게시글 단일 조회 api
     public Board showPost(Long id) {
         Board board = boardRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 개시글을 찾을 수 없습니다"));
         return board;
     }
 
-    //게시글 수정 //updatedDate도 추가해야함 //멤버아이디도 있어야함
+    //게시글 수정 api
     public Board modifiedPost(Long id,Long memberId, BoardRequestDto boardRequestDto) {
         Board board = showPost(id);
         //보드를 받아옴 //엔티티 클래스에서 setter 사용을 지양하자! -> 해당 클래스의 인스턴스 값들이 언제 어디서 변해야 하는지 구분하기 어려워짐
@@ -70,6 +69,7 @@ public class BoardService {
             throw new IllegalArgumentException("로그인 상태가 아닙니다.");
         }
         Member member = memberRepository.findById(memberRepository.findById(memberId).get().getId()).get();
+
         BoardResponseDto boardResponseDto = new BoardResponseDto(board.getId(), boardRequestDto.getTitle(), boardRequestDto.getContent());
         boardResponseDto.setCreatedDate(board.getCreatedDate()); // 생성일자는 유지해놔야하기 때문에
         boardResponseDto.setUpdatedDate(LocalDateTime.now());
@@ -78,7 +78,7 @@ public class BoardService {
         return modifiedBoard;
     }
 
-    //게시글 삭제
+    //게시글 삭제 api
     public void deletePost(Long id,Long memberId){
         if(!memberRepository.existsById(memberRepository.findById(memberId).get().getId())){
             throw new IllegalArgumentException("로그인 상태가 아닙니다.");
